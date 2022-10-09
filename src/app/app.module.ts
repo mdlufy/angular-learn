e45a756e8844b6d94684a11650a409a0b9886b66
mdlufy <german.panov.2018@mail.ru>
2022-10-09 22:49:46 +0300

    feat: product detail component and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,24 @@ import {AppComponent} from './app.component';
 import {ProductListComponent} from './product-list/product-list.component';
 import {TopBarComponent} from './top-bar/top-bar.component';
 import {ProductAlertsComponent} from './product-alerts/product-alerts.component';
+import {ProductDetailsComponent} from './product-details/product-details.component';
 
 @NgModule({
     imports: [
         BrowserModule,
         AppRoutingModule,
         ReactiveFormsModule,
-        RouterModule.forRoot([{path: '', component: ProductListComponent}]),
+        RouterModule.forRoot([
+            {path: '', component: ProductListComponent},
+            {path: 'products/:productId', component: ProductDetailsComponent},
+        ]),
     ],
     declarations: [
         AppComponent,
-        ProductListComponent,
         TopBarComponent,
+        ProductListComponent,
         ProductAlertsComponent,
+        ProductDetailsComponent,
     ],
     providers: [],
     bootstrap: [AppComponent],
