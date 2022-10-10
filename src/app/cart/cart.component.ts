96db3f76e9efbaa716ec3d07c9bdb8991816a82e
mdlufy <german.panov.2018@mail.ru>
2022-10-10 14:11:01 +0300

    feat: chechout form

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import {CartService} from './../cart.service';
 import {Component, OnInit} from '@angular/core';
+import {FormBuilder} from '@angular/forms';
 
 @Component({
     selector: 'app-cart',
@@ -9,5 +10,21 @@ import {Component, OnInit} from '@angular/core';
 export class CartComponent {
     items = this.cartService.getItems();
 
-    constructor(private cartService: CartService) {}
+    checkoutForm = this.formBuilder.group({
+        name: '',
+        address: '',
+    });
+
+    constructor(
+        private cartService: CartService,
+        private formBuilder: FormBuilder
+    ) {}
+
+    onSubmit(): void {
+        this.items = this.cartService.clearCart();
+
+        console.warn('Your order has been submitted', this.checkoutForm.value);
+
+        this.checkoutForm.reset();
+    }
 }
