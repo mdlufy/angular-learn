c4efd8f0b591a7cb646fe52bfe47dc4b757da0a5
mdlufy <german.panov.2018@mail.ru>
2022-10-10 12:43:26 +0300

    feat: shopping cart

---
+++ src/app/cart/cart.component.ts
@@ -0,0 +1,13 @@
+import {CartService} from './../cart.service';
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'app-cart',
+    templateUrl: './cart.component.html',
+    styleUrls: ['./cart.component.scss'],
+})
+export class CartComponent {
+    items = this.cartService.getItems();
+
+    constructor(private cartService: CartService) {}
+}
--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,3 +1,4 @@
+import {CartService} from './../cart.service';
 import {Product, products} from './../products';
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
@@ -10,7 +11,10 @@ import {ActivatedRoute} from '@angular/router';
 export class ProductDetailsComponent implements OnInit {
     product: Product | undefined;
 
-    constructor(private route: ActivatedRoute) {}
+    constructor(
+        private route: ActivatedRoute,
+        private cartService: CartService
+    ) {}
 
     ngOnInit() {
         const routeParams = this.route.snapshot.paramMap;
@@ -19,5 +23,10 @@ export class ProductDetailsComponent implements OnInit {
         this.product = products.find(
             (product) => product.id === productIdFromRoute
         );
+    }
+
+    addToCart(product: Product) {
+        this.cartService.addToCart(product);
+        window.alert('Your product has been added to the cart!');
     }
 }
